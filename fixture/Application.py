fc381a69bf510e9314fceabba507a8bae9d1502f
Oleg <prostooleg1@gmail.com>
2017-06-09 21:12:35 +0200

    add firsts structure for project mantis

---
+++ fixture/Application.py
@@ -0,0 +1,29 @@
+
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "safari":
+            self.wd = webdriver.Safari()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        # self.wd.implicitly_wait(10)
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def destroy(self):
+        self.wd.quit()
---
+++ fixture/session.py
@@ -0,0 +1,52 @@
+class SessionHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def login(self, user, password):
+        wd = self.app.wd
+        self.open_home_page()
+        wd.find_element_by_name("username").click()
+        wd.find_element_by_name("username").clear()
+        wd.find_element_by_name("username").send_keys(user)
+        wd.find_element_by_name("password").click()
+        wd.find_element_by_name("password").clear()
+        wd.find_element_by_name("password").send_keys(password)
+        wd.find_element_by_css_selector('input[type="submit"]').click()
+
+    def logout(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("Logout").click()
+
+    def ensure_logout(self):
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
+
+    def get_logged_user(self):
+        wd = self.app.wd
+        return wd.find_element_by_css_selector("td.login-info-left span").text
+
+    def is_logged_in_as(self, username):
+        return self.get_logged_user() == username
+
+    def open_home_page(self):
+        wd = self.app.wd
+        if wd.current_url.endswith("/index.php") and len(wd.find_elements_by_name("add")) > 0:
+            return
+        wd.get(self.app.base_url)
+
+
+
