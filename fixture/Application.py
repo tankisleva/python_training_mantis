f15c411bb22232e9b858034d0d3fb9bef8951ec3
Oleg <prostooleg1@gmail.com>
2017-06-10 11:50:06 +0200

    add suds

--- fixture/Application.py
+++ fixture/Application.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
+from fixture.soap import SoapHelper
 
 
 class Application:
@@ -18,6 +19,7 @@ class Application:
         # self.wd.implicitly_wait(10)
         self.session = SessionHelper(self)
         self.project = ProjectHelper(self)
+        self.soap = SoapHelper(self)
         self.base_url = base_url
 
     def is_valid(self):
--- test/test_login.py
+++ test/test_login.py
@@ -2,3 +2,4 @@
 def test_login(app):
     app.session.login("administrator", "root")
     assert app.session.is_logged_in_as("administrator")
+    assert app.soap.can_login("administrator", "root")
